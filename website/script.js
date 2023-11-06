cb13ae5e4f3f3fad289b1968e105fda795a58246
joluhan <135083872+joluhan@users.noreply.github.com>
2023-11-06 17:14:17 +0700

    reworked website

--- website/script.js
+++ website/script.js
@@ -1,143 +1,125 @@
-// TO DO
-// make sure to fetch the 7 best movies + also for categories
-
 document.addEventListener('DOMContentLoaded', function () {
-    const apiBaseUrl = 'http://localhost:8000/api/v1';
-    
-    async function fetchData(endpoint) {
-      try {
-        const response = await fetch(`${apiBaseUrl}${endpoint}`);
-        return await response.json();
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
+  const baseURL = 'http://localhost:8000/api/v1/titles/';
 
-    // Render movie details
-    function renderMovieDetails(sectionId, movies, genre) {
-      const section = document.getElementById(sectionId); //change section
-      section.innerHTML = `<h2>${genre}</h2>`;
-      
-      // Create a div for the carousel
-      const carouselDiv = document.createElement('div');
-      carouselDiv.classList.add('categoryMovies');
+  // Fetch movies and return JSON data
+  function fetchMovies(url) {
+      return fetch(url).then(response => response.json());
+  }
 
-      movies.forEach(movie => {
-        const movieDiv = document.createElement('div');
-        movieDiv.classList.add('movie', 'glass-box'); // Added 'glass-box'
-
-        movieDiv.innerHTML = `
-          <img src="${movie.image_url}" alt="${movie.title}" class="movie-image">
-          <h3 class="movie-title">${movie.title}</h3>
-          <p class="movie-resume">${movie.summary}</p>
-          <button class="details-button" data-movie-id="${movie.id}">Details</button>
-        `;
-
-        carouselDiv.appendChild(movieDiv);  // Append movieDiv to carouselDiv
+  // Display the best movie in its section
+  function displayBestMovie() {
+      fetchMovies(`${baseURL}?sort_by=-imdb_score`).then(data => {
+          const bestMovie = data.results[0];
+          fetchMovies(`${baseURL}${bestMovie.id}`).then(movie => {
+              const bestMovieElement = createMovieElement(movie, true);
+              document.getElementById('bestMovieDetails').appendChild(bestMovieElement);
+          });
       });
+  }
 
-      section.appendChild(carouselDiv);  // Append carouselDiv to section
-    }
-
-    // Render modal with movie details ------- ADD ERROR MANAGEMENT IF NO IMG AVAILABLE
-    function renderModal(movieDetails) {
-        const modal = document.getElementById('movieModal');
-        modal.innerHTML = `
-          <img src="${movieDetails.image_url}" alt="${movieDetails.title}" class="movie-image">
-          <div class="movie-info">
-          <h2>${movieDetails.title}</h2>
-          <p><strong>Genre:</strong> ${movieDetails.genre}</p>
-          <p><strong>Date de sortie:</strong> ${movieDetails.date_published}</p>
-          <p><strong>Rated:</strong> ${movieDetails.rated}</p>
-          <p><strong>IMDb Score:</strong> ${movieDetails.imdb_score}</p>
-          <p><strong>Réalisateur:</strong> ${movieDetails.director}</p>
-          <p><strong>Acteurs:</strong> ${movieDetails.actors.join(', ')}</p>
-          <p><strong>Durée:</strong> ${movieDetails.duration} mins</p>
-          <p><strong>Pays d'origine:</strong> ${movieDetails.countries.join(', ')}</p>
-          <p><strong>Box Office:</strong> ${movieDetails.box_office}</p>
-          <p><strong>Résumé:</strong> ${movieDetails.description}</p>
-          </div>
-          <button id="closeModal">Close</button>
-        `;
-        
-        // Add event listener to close modal
-        document.getElementById('closeModal').addEventListener('click', () => {
-          modal.style.display = 'none';
+  // Display movies in a given section
+  function displayMoviesInSection(url, sectionId, sectionTitle) {
+      fetchMovies(url).then(data => {
+        const section = document.getElementById(sectionId);
+        const carousel = section.querySelector('.carousel');
+        data.results.forEach(movie => {
+            const movieElement = createMovieElement(movie);
+            carousel.appendChild(movieElement);
         });
-      }
-    // Handle click on details button
-    function handleDetailsButtonClick(event) {
-      const movieId = event.target.dataset.movieId;
-      if (movieId) {
-        fetchMovieDetails(movieId);
-      }
-    }
+        section.querySelector('h2').innerText = sectionTitle;
 
-    // function addArrows(sectionId) {
-    //   const section = document.getElementById(sectionId);
-
-    //   const arrowLeft = document.createElement('div');
-    //   const arrowRight = document.createElement('div');
-
-    //   arrowLeft.classList.add('arrow', 'arrow-left');
-    //   arrowRight.classList.add('arrow', 'arrow-right');
-
-    //   section.prepend(arrowLeft);
-    //   section.appendChild(arrowRight);
-    
-    //   arrowLeft.addEventListener('click', function() {
-    //     section.scrollBy({ left: -1000, behavior: 'smooth' });
-    //   });
-      
-    //   arrowRight.addEventListener('click', function() {
-    //     section.scrollBy({ left: 1000, behavior: 'smooth' });
-    //   });
-    // }
-    
-    // addArrows('topRatedMovies');
-    // addArrows('category1Movies');
-    // addArrows('category2Movies');
-    // addArrows('category3Movies');
-
-    
-    // Fetch movie details and render modal 
-    async function fetchMovieDetails(movieId) {
-      const movieDetails = await fetchData(`/titles/${movieId}`);
-      renderModal(movieDetails);
-      document.getElementById('movieModal').style.display = 'flex';
-    }
-  
-    // Add event listener to document to handle click on details button
-    document.addEventListener('click', function(event) {
-      if (event.target.classList.contains('details-button')) {
-        handleDetailsButtonClick(event);
-      }
     });
-  
-    // Fetch data for best movie and top rated movies
-    fetchData('/titles/?sort_by=-imdb_score&limit=8').then(data => {
-      const [bestMovie, ...topRatedMovies] = data.results;
-      renderMovieDetails('bestMovieDetails', [bestMovie], 'Best Movie');
-     // Special Treatment for Best Movie
-      const bestMovieDiv = document.getElementById('bestMovieDetails');
-      bestMovieDiv.classList.remove('categoryMovies');  // Remove categoryMovies class
-      bestMovieDiv.firstChild.classList.add('glass-box');  // Add 'glass-box'
+}
 
-      renderMovieDetails('topRatedMovies', topRatedMovies, 'Top Rated Movies');
-    });
-    
-    // Define categories
-    const categories = [
-      {endpoint: '/titles/?genre=action&sort_by=-imdb_score&limit=7', sectionId: 'category1Movies', genre: 'Action'},
-      {endpoint: '/titles/?genre=drama&sort_by=-imdb_score&limit=7', sectionId: 'category2Movies', genre: 'Drama'},
-      {endpoint: '/titles/?genre=comedy&sort_by=-imdb_score&limit=7', sectionId: 'category3Movies', genre: 'Comedy'}
-    ];
-  
-    // Fetch data for each category and render it
-    categories.forEach(category => {
-      fetchData(category.endpoint).then(data => {
-        renderMovieDetails(category.sectionId, data.results, category.genre);
+  // Create a movie element for the DOM
+  function createMovieElement(movie, isBestMovie = false) {
+      const movieDiv = document.createElement('div');
+      movieDiv.className = isBestMovie ? 'best-movie' : 'movie-glass-box';
+      movieDiv.innerHTML = `
+          <div class='movie-image'>
+              <img src="${movie.image_url}" alt="${movie.title}" class="movie-image" onerror="changeImageOnError(this)" />
+          </div>
+          <div class="movie-details">
+              <h3>${movie.title}</h3>
+              ${isBestMovie ? `<p>${movie.description}</p>` : ''}
+              <button class="details-button" onclick="openModal(${movie.id})">Détails</button>
+          </div>
+      `;
+      return movieDiv;
+  }
+
+  // Function to open a modal with movie details
+  window.openModal = function (movieId) {
+      fetchMovies(`${baseURL}${movieId}`).then(movie => {
+          const modal = document.getElementById('movieModal');
+          modal.style.display = 'block';
+          modal.querySelector('.modal-content').innerHTML = `
+              <span class="close" onclick="closeModal()">&times;</span>
+              <h2>${movie.title}</h2>
+              <img src="${movie.image_url}" alt="${movie.title}" class="movie-image" />
+              <p><strong>Genre:</strong> ${movie.genres.join(', ')}</p>
+              <p><strong>Date Published:</strong> ${movie.date_published}</p>
+              <p><strong>Rating:</strong> ${movie.rated}</p>
+              <p><strong>IMDb Score:</strong> ${movie.imdb_score}</p>
+              <p><strong>Director:</strong> ${movie.directors.join(', ')}</p>
+              <p><strong>Actors:</strong> ${movie.actors.join(', ')}</p>
+              <p><strong>Duration:</strong> ${movie.duration} minutes</p>
+              <p><strong>Country:</strong> ${movie.countries.join(', ')}</p>
+              <p><strong>Box Office:</strong> ${movie.worldwide_gross_income ? movie.worldwide_gross_income : 'N/A'}</p>
+              <p><strong>Summary:</strong> ${movie.long_description}</p>
+          `;
       });
-    });
-  });
-  
+  };
+
+  // Function to close the modal
+  window.closeModal = function () {
+      const modal = document.getElementById('movieModal');
+      modal.style.display = 'none';
+  };
+
+  // Function to set up carousel scrolling
+  function setupCarouselScrolling() {
+      const carousels = document.querySelectorAll('.movie-carousel');
+      carousels.forEach(carousel => {
+          const prevButton = carousel.querySelector('.prev-button');
+          const nextButton = carousel.querySelector('.next-button');
+          const carouselContainer = carousel.querySelector('.carousel');
+
+          prevButton.addEventListener('click', () => {
+              carouselContainer.scrollLeft -= carouselContainer.offsetWidth / 4;
+          });
+
+          nextButton.addEventListener('click', () => {
+              carouselContainer.scrollLeft += carouselContainer.offsetWidth / 4;
+          });
+      });
+  }
+
+  // Display movies for each category
+  function displayCategoryMovies() {
+      // Define categories and corresponding section IDs
+      const categories = [
+          { name: 'Action', section: 'category1Movies' },
+          { name: 'Drama', section: 'category2Movies' },
+          { name: 'Comedy', section: 'category3Movies' }
+      ];
+
+      // Fetch and display movies for each category
+      categories.forEach(category => {
+          const url = `${baseURL}?genre=${category.name}&sort_by=-imdb_score&page_size=7`;
+          displayMoviesInSection(url, category.section, category.name);
+      });
+  }
+
+  // Initialize the page by displaying movies
+  displayBestMovie();
+  displayMoviesInSection(`${baseURL}?sort_by=-imdb_score&page_size=7`, 'topRatedMovies', 'Films Les Mieux Notés');
+  displayCategoryMovies();
+  setupCarouselScrolling();
+});
+
+function changeImageOnError(image) {
+  // Change the image source URL to the backup image
+  // image.src = "placeholder.jpg";
+  image.remove();
+}
